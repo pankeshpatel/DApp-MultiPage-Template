89c1c42571b84154387c28cb57baf8f8dedb8053
pankeshpatel <pankeshlinux@gmail.com>
2019-07-18 13:30:17 +0530

    revised compile.js file

--- README.md
+++ README.md
@@ -24,20 +24,18 @@
 - `README.md`             --  readme file of the project
 - `node_modules`          --  project dependencies
 
-# dependencies installation
+# Installation
 
 - `npm init` to generate `package.json` file
 - `npm install --save ganache-cli mocha solc@0.4.17 fs-extra web3@1.0.0-beta.35` to install dependencies
 
-> Write operation in test file
-
-`await factory.methods.createCampaign('100').send({
-  from: accounts[0],
-  gas: '1000000'
-});`
-
-
-
+# User Guide
+- Step 1: write smart contract (`.sol` file) into Remix Editor
+- Step 2: copy paste the smart contract (`.sol` file) into a file of `contracts` directory.
+- Step 3: compile the contract using `compile.js`
+- Step 4: test the contract using a file of a `test` directory. The contract is deployed on local network `ganache`
+- Step 5: deploy the contract on a test network (e.g., Rinkeby).
+- Step 6: extract the address of the deployed contract from console.
 
 ### WebApp and Smart Contract Interaction ###
 
--- test/Campaign.test.js
+++ test/Campaign.test.js
@@ -1,10 +1,10 @@
-const assert = require('assert');
-const ganache = require('ganache-cli');
-const Web3 = require('web3');
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
 
-const compiledFactory = require('../ethereum/build/CampaignFactory.json');
-const compiledCampaign = require('../ethereum/build/Campaign.json');
+const compiledFactory = require("../ethereum/build/CampaignFactory.json");
+const compiledCampaign = require("../ethereum/build/Campaign.json");
 
 let accounts;
 let factory;
@@ -17,7 +17,7 @@ beforeEach(async () => {
   // This deploy the contract
   factory = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
     .deploy({ data: compiledFactory.bytecode })
-    .send({ from: accounts[0], gas: '1000000' });
+    .send({ from: accounts[0], gas: "1000000" });
 
   // Call creatCampaign() method of the deployed contract
 
@@ -27,9 +27,9 @@ beforeEach(async () => {
   // Read operation --
   // Call() is used whenever, we try to call a function of a smart contract.
   // In call(), there is not data modification.
-  await factory.methods.createCampaign('100').send({
+  await factory.methods.createCampaign("100").send({
     from: accounts[0],
-    gas: '1000000'
+    gas: "1000000"
   });
 
   //Call getDeployedCampaigns() method of the deployed contract.
@@ -42,30 +42,30 @@ beforeEach(async () => {
   );
 });
 
-describe('Campaigns', () => {
-  it('deploys a factory and a campaign', () => {
+describe("Campaigns", () => {
+  it("deploys a factory and a campaign", () => {
     assert.ok(factory.options.address);
     assert.ok(campaign.options.address);
   });
 
-  it('marks caller as the campaign manager', async () => {
+  it("marks caller as the campaign manager", async () => {
     const manager = await campaign.methods.manager().call();
     assert.equal(accounts[0], manager);
   });
 
-  it('allows people to contribute money and marks them as approvers', async () => {
+  it("allows people to contribute money and marks them as approvers", async () => {
     await campaign.methods.contribute().send({
-      value: '200',
+      value: "200",
       from: accounts[1]
     });
     const isContributor = await campaign.methods.approvers(accounts[1]).call();
     assert(isContributor);
   });
 
-  it('requires a minimum contribution', async () => {
+  it("requires a minimum contribution", async () => {
     try {
       await campaign.methods.contribute().send({
-        value: '5',
+        value: "5",
         from: accounts[1]
       });
       assert(false);
@@ -74,40 +74,40 @@ describe('Campaigns', () => {
     }
   });
 
-  it('allows a manager to make a payment request', async () => {
+  it("allows a manager to make a payment request", async () => {
     await campaign.methods
-      .createRequest('Buy batteries', '100', accounts[1])
+      .createRequest("Buy batteries", "100", accounts[1])
       .send({
         from: accounts[0],
-        gas: '1000000'
+        gas: "1000000"
       });
     const request = await campaign.methods.requests(0).call();
 
-    assert.equal('Buy batteries', request.description);
+    assert.equal("Buy batteries", request.description);
   });
 
-  it('processes requests', async () => {
+  it("processes requests", async () => {
     await campaign.methods.contribute().send({
       from: accounts[0],
-      value: web3.utils.toWei('10', 'ether')
+      value: web3.utils.toWei("10", "ether")
     });
 
     await campaign.methods
-      .createRequest('A', web3.utils.toWei('5', 'ether'), accounts[1])
-      .send({ from: accounts[0], gas: '1000000' });
+      .createRequest("A", web3.utils.toWei("5", "ether"), accounts[1])
+      .send({ from: accounts[0], gas: "1000000" });
 
     await campaign.methods.approveRequest(0).send({
       from: accounts[0],
-      gas: '1000000'
+      gas: "1000000"
     });
 
     await campaign.methods.finalizeRequest(0).send({
       from: accounts[0],
-      gas: '1000000'
+      gas: "1000000"
     });
 
     let balance = await web3.eth.getBalance(accounts[1]);
-    balance = web3.utils.fromWei(balance, 'ether');
+    balance = web3.utils.fromWei(balance, "ether");
     balance = parseFloat(balance);
 
     assert(balance > 104);
